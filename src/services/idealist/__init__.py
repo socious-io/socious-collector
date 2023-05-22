1a0b5c515c0166a5043209ceede3fa8cc47f8071
jeyem <me@e-mahmoudi.me>
2023-05-22 22:24:02 +0330

    enable all idealist jobs

--- src/services/idealist/__init__.py
+++ src/services/idealist/__init__.py
@@ -2,12 +2,12 @@ from .jobs import IdealistListingJob, IdealistRowQueue
 
 JOBS = (
     IdealistListingJob('jobs'),
-    # IdealistListingJob('volops'),
-    # IdealistListingJob('internships'),
+    IdealistListingJob('volops'),
+    IdealistListingJob('internships'),
 )
 
 QUEUES = (
     IdealistRowQueue('jobs'),
-    # IdealistRowQueue('volops'),
-    # IdealistRowQueue('internships'),
+    IdealistRowQueue('volops'),
+    IdealistRowQueue('internships'),
 )
